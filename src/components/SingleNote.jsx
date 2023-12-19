53bf9e0411788c5a8677402dc903c216d684fc05
Kevin Webster Revalde <96121161+kevsterde@users.noreply.github.com>
2023-12-19 16:56:12 +0800

    Update SingleNote.jsx

--- src/components/SingleNote.jsx
+++ src/components/SingleNote.jsx
@@ -13,7 +13,7 @@ function SingleNote(props) {
     <>
     <div className="note-container">
     <h3 className="title">{props.title}</h3>
-    <p className="content">{props.content}</p>
+    <pre className="content">{props.content}</pre>
     <button className='delete' onClick={()=>handleClick(props.id)}>Delete</button>
     <button className='delete' onClick={()=>UpdateClick({content:props.content,title:props.title,id:props.id})}>Update</button>
   </div>
